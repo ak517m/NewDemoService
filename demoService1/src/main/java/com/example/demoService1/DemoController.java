a3dc71918d91f52510f9652129f3ea8068f31034
Abhishek <abhishek21111987@gmail.com>
2020-01-02 15:05:22 -0500

    commiteed

--- demoService1/src/main/java/com/example/demoService1/DemoController.java
+++ demoService1/src/main/java/com/example/demoService1/DemoController.java
@@ -1,9 +1,23 @@
 package com.example.demoService1;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class DemoController {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+	@GetMapping("/employees")
+	public String retrieveAddressForEmployee() {
+		return "Abhishek....";
 
 	}
 
